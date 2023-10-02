01f89083e0f6ea6631eb7df99088233e0ef4ce86
Carljul <carljulamles@gmail.com>
2023-10-02 17:12:11 +0800

    [UPDATE] - Barcode

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php include "layouts/header.php"?>
-  <a href="pages/terms" class="btn danger">Terms</a>
-  <a href="pages/registration" class="btn danger">Register</a>
-  <a href="pages/qr-reading" class="btn danger">QR Reading</a>
+  <div class="scrollable-y flex gap-10">
+    <a href="pages/terms" class="btn danger">Terms</a>
+    <a href="pages/registration" class="btn danger">Register</a>
+    <a href="pages/qr-reading" class="btn danger">QR Reading</a>
+    <a href="pages/barcode" class="btn danger">Barcode</a>
+  </div>
+  <br>
+  <br>
   <hr>
   <h1>Docs</h1>
   <h4>How to include navbar to each page?</h4>
@@ -23,5 +28,17 @@
     color: #fff;
   ">
     npm i html5-qrcode
+  </pre>
+  
+  <h4>Header is not needed but need to read its css?</h4>
+  <span>Put this to the top of your code to remove header but still able to use glico.css</span>
+  <pre style="
+    background: black;
+    padding-top: 10px;
+    color: #fff;
+  ">
+    &lt;?php
+      $header = false;
+    ?&gt;
   </pre>
 <?php include "layouts/footer.php"?>
